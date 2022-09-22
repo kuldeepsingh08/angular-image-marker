17321c57749777d6e9f9bbcac1af72549a28cc80
kuldeepsingh08 <102313793+kuldeepsingh08@users.noreply.github.com>
2022-09-22 18:57:09 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-kykc4m
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-kykc4m)
---
+++ src/app/app.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit, VERSION } from '@angular/core';
+
+interface MATRIX_ARR  {
+  id: number;
+  isMarked: boolean;
+}
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent implements OnInit  {
+  name = 'Angular ' + VERSION.major;
+  items = Array.from(Array(11).keys());
+  grid = Array.from(Array(9).keys());
+  itemsArr = [];
+  gridArr = [];
+  matrixArr = [];
+  
+  ngOnInit(): void {
+    // const myImg = document.querySelector("#my-img");
+    // console.log('check>>>', myImg.clientWidth);
+    this.items.forEach(element => this.itemsArr.push({ id: element, isMarked: false }))
+    this.grid.forEach(element => this.gridArr.push({ id: element, isMarked: false }))
+    this.grid.forEach((element, i) => {
+      this.matrixArr.push([]);
+      this.items.forEach(element2 => {
+        this.matrixArr[i].push([{ x: element2, y: element }])
+      })
+    });
+    console.log('checkMatrix', this.matrixArr)
+  }
+
+  checkPosition(x: number, y: number, isMarked: boolean): void {
+    this.gridArr[x].isMarked = !this.gridArr[x].isMarked;
+    this.itemsArr[y].isMarked = !this.itemsArr[y].isMarked;
+    console.log('x,y: ', x,y, isMarked);
+  }
+
+  checkPositionNew(cell: any): void {
+    console.log('cell', cell);
+  }
+}
